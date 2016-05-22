8b4fbaf0e6be0384c34e61ea94eec30740f620d3
Jon Gold <hello@designedbygold.com>
2016-05-22 14:00:06 +0100

    clean up coin-change

--- src/greedy_algorithms/coin_change.clj
+++ src/greedy_algorithms/coin_change.clj
@@ -6,11 +6,10 @@
    (coin-change amt (reverse (sort coins)) []))
 
   ([amt coins used]
-   (if (empty? coins)
-     (if (> amt 0) [] (reverse used))
-     (let [biggest (first coins)]
-       (if (> biggest amt)
-         ;; first coin won't fit so new sub-problem doesn't consider it
-         (recur (+ amt 0) (rest coins) used)
-         ;; if it will fit use it - we reduce the amount and store and coin
-         (recur (- amt biggest) coins (conj used biggest)))))))
+   (let [biggest (first coins)]
+     (cond
+       (empty? coins) (if (> amt 0) [] (reverse used))
+       ;; first coin won't fit so new sub-problem doesn't consider it
+       (> biggest amt) (recur (+ amt 0) (rest coins) used)
+       ;; if it will fit use it - we reduce the amount and store and coin
+       :else (recur (- amt biggest) coins (conj used biggest))))))
