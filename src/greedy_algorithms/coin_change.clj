3d37a6f2189e04d238619564071af72eba26fb1d
Jon Gold <hello@designedbygold.com>
2016-05-23 13:45:52 +0100

    tidy

--- src/greedy_algorithms/coin_change.clj
+++ src/greedy_algorithms/coin_change.clj
@@ -8,8 +8,10 @@
   ([amt coins used]
    (let [biggest (first coins)]
      (cond
-       (empty? coins) (if (> amt 0) [] (reverse used))
+      (empty? coins)   (if (> amt 0)
+                         []
+                         (reverse used))
        ;; first coin won't fit so new sub-problem doesn't consider it
        (> biggest amt) (recur (+ amt 0) (rest coins) used)
        ;; if it will fit use it - we reduce the amount and store and coin
-       :else (recur (- amt biggest) coins (conj used biggest))))))
+       :else           (recur (- amt biggest) coins (conj used biggest))))))
--- src/greedy_algorithms/schedule_events.clj
+++ src/greedy_algorithms/schedule_events.clj
@@ -8,10 +8,9 @@
 (defn schedule-events
   ([inbox] (schedule-events (sort-by :finish inbox) []))
   ([inbox cal]
-   (let [next (first inbox)
-         last-sched (last cal)]
+   (let [next (first inbox) last-sched (last cal)]
      (cond
        (empty? inbox) cal
-       (empty? cal) (recur (rest inbox) (conj cal next))
+       (empty? cal)               (recur (rest inbox) (conj cal next))
        (overlaps next last-sched) (recur (rest inbox) cal)
-       :else (recur (rest inbox) (conj cal next))))))
+       :else (recur (rest inbox)  (conj cal next))))))
